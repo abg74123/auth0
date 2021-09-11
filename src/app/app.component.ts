ec1880ac58602eae7e3daa7813f1efe0647332d3
abg74123 <bisza_i.d@hotmail.com>
2021-09-11 22:22:29 +0700

    create auth0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import data from './data.json'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'rxjs';
+export class AppComponent implements OnInit,OnDestroy {
+  data = data
+  constructor(public auth:AuthService){
+
+  }
+
+  ngOnInit(){
+    
+  }
+
+  login(){
+    this.auth.loginWithRedirect()
+  }
+
+  loout(){
+    this.auth.logout({returnTo:window.location.origin})
+  }
+
+  ngOnDestroy(){
+    // this.destroy$.next()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AuthModule } from '@auth0/auth0-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { environment as env } from '../environments/environment';
+import { ProfileComponent } from './profile/profile.component';
+import {SkeletonModule} from 'primeng/skeleton';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SkeletonModule,
+    AuthModule.forRoot({
+      ...env.auth
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
